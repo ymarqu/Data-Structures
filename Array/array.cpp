fd32b6bbf40550eaf2aa77d8c4be5b6a4baeee12
ymarquina <yeseniamrqna@gmail.com>
2023-04-18 22:40:39 -0700

    Add Linear search and binary search function

--- Array/array.cpp
+++ Array/array.cpp
@@ -37,6 +37,41 @@ void Delete(struct Array *arr, int index){
     }
 }
 
+void swap(int *x, int *y){
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int LinearSearch(struct Array *arr, int x){
+    for(int i = 0; i < arr->length; i++){
+        if(arr->A[i] == x){
+            swap(arr->A[i], arr->A[i-1]);
+            return i;
+        }
+    }
+    return -1;
+}
+
+int binarySearch(struct Array arr, int x){
+    int high = arr.length -1;
+    int low = 0;
+
+    while(low <= high){
+        int mid = high + low / 2;
+
+        if(arr.A[mid] == x){
+            return mid;
+        }else if(arr.A[mid] > x){
+            high = mid -1;
+        }else{
+            low = mid + 1;
+        }
+    }
+    return -1;
+}
+
+
 int main(){
 
     struct Array arr={{2,3,4,5,6}, 20, 5};
@@ -47,11 +82,23 @@ int main(){
 
     Display(arr);
 
-    Insert(&arr, 4, 15);
+    Insert(&arr, 5, 15);
 
     Display(arr);
 
-    Delete(&arr, 0);
+    // Delete(&arr, 0);
+
+    Display(arr);
+
+    cout << LinearSearch(&arr, 4) << endl;
+
+    Display(arr);
+
+    cout << binarySearch(arr, 4) << endl;;
+
+    cout << binarySearch(arr, 10) << endl;
+
+    cout << binarySearch(arr, 20) << endl;
 
     Display(arr);
 
