d35039dcf1c3bc2919c46c42c6ebe18daca0ea2c
ymarquina <yeseniamrqna@gmail.com>
2023-06-06 15:41:16 -0700

    Add function definition for Add, remove, and print

--- doublyLinkedList/doubly.cpp
+++ doublyLinkedList/doubly.cpp
@@ -9,20 +9,74 @@
     count = 0;
    }
    void doublyLinkedList::insertAtFront(int elem){
-
-
+    if(head == NULL){
+       head = new DLLNode;
+       head->prev = NULL;
+       head->next = NULL;
+       head->data = elem;
+       tail = head;
+   }else{
+    head->prev = new DLLNode;
+    head->prev->next = head;
+    head = head->prev;
+   }
+    count++;
    }
     void doublyLinkedList::insertAtpos(int pos, int elem){
 
     }
     void doublyLinkedList::insertAtTail(int elem){
-
+    if(head == NULL){
+       head = new DLLNode;
+       head->prev = NULL;
+       head->next = NULL;
+       head->data = elem;
+       tail = head;
+    }else{
+       tail->next = new DLLNode;
+       tail->next->prev = tail;
+       tail = tail->next;
     }
-    int doublyLinkedList::deleteFromHead(){
+    count++;
+   }
 
+    int doublyLinkedList::deleteFromHead(){
+        if(isEmpty()){
+            return -1;
+        }
+        int elem = head->data;
+        if(head == tail){
+            DLLNode *temp = head;
+            head = NULL; tail = NULL;
+            delete temp;
+        }else{
+            DLLNode *temp = head;
+            head = head->next;
+            temp->next = NULL;
+            head->prev = NULL;
+            delete temp;
+        }
+        count--;
+        return elem;
     }
     int doublyLinkedList::deleteFromTail(){
-
+     if(isEmpty()){
+        return -1;
+     }
+     int elem = tail ->data;
+     if(head == tail){
+            DLLNode *temp = head;
+            head = NULL; tail = NULL;
+            delete temp;
+        }else{
+            DLLNode *temp = tail;
+            tail = tail->prev;
+            temp->prev = NULL;
+            tail->next = NULL;
+            delete temp;
+        }
+        count--;
+        return elem;
     }
     int doublyLinkedList::deleteAtPos(int pos){
 
@@ -32,7 +86,40 @@
     }
     void doublyLinkedList::printList(){
 
+        if(isEmpty()){
+            cout << "List is empty. Cannot print." << endl;
+        }else{
+            DLLNode *temp = head;
+            while(temp != NULL){
+                cout << "[";
+                cout << temp->data;
+                cout <<"]->";
+                temp = temp->next;
+            }
+            cout << "x" << endl;
+            temp = NULL;
+            delete temp;
+        }
     }
     void doublyLinkedList::printListReverse(){
-
+        if(isEmpty()){
+            cout << "List is empty. Cannot print." << endl;
+        }else{
+            DLLNode *temp = tail;
+            while(temp != NULL){
+                cout << "[";
+                cout << temp->data;
+                cout << "]->";
+                temp = temp->prev;
+            }
+            cout << "x" << endl;\
+            temp = NULL;
+            delete temp;
+        }
+    }
+    bool isEmpty(){
+        if(count == 0){
+            return true;
+        }
+        return false;
     }
